fee06fdfeddc752e6c3b7a648268fdd1da2dfc19
Diandra <leao.diandra@gmail.com>
2021-04-24 19:35:17 -0300

    add estilos ao componente

--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Comentario.css';
 
 // JSX -> html dentro do JS
 const Comentario = () => (
-    <>
+    <div className="comentario">
         <div>Diandra:</div>
         <div>Olá, eu sou um componente</div>
-    </>
+    </div>
 );
 
 export default Comentario
