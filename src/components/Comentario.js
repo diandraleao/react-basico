8fc37d2d0cf40d23cc1e10334584968eef2942a9
Diandra <leao.diandra@gmail.com>
2021-04-23 21:16:48 -0300

    comentario com fragmentos

---
+++ src/components/Comentario.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// JSX -> html dentro do JS
+const Comentario = () => (
+    <>
+        <div>Diandra:</div>
+        <div>Olá, eu sou um componente</div>
+    </>
+);
+
+export default Comentario
