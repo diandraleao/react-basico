ee3a61aa2569cb472d0fa684cc9f43379b61fb84
Diandra <leao.diandra@gmail.com>
2021-04-25 17:02:05 -0300

    adicionando estado da aplicacao

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
+import React, { Component } from 'react';
 import './App.css';
-import Comentario from './components/Comentario'
+import Comentario from './components/Comentario';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Meu projeto</h1>
-      <Comentario nome="Diandra" email="diandra@gmail.com" data={new Date(2021, 3, 21)}>
-        Olá, tudo bem?
-      </Comentario>
 
-      <Comentario nome="Maria" email="maria@gmail.com" data={new Date(2021, 3, 24)}>
-        Olá, sim.
-      </Comentario>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    comentarios: [
+      {
+        nome: 'Joao',
+        email: 'joao@mail.com',
+        data: new Date(2021, 3, 19),
+        mensagem: 'Ola'
+      },
+      {
+        nome: 'Maria',
+        email: 'maria@mail.com',
+        data: new Date(2021, 3, 19),
+        mensagem: 'Ola'
+      },
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Meu projeto</h1>
+
+        {this.state.comentarios.map((comentario, idx) => (
+          <Comentario 
+            key={idx}
+            nome={comentario.nome} 
+            email={comentario.email} 
+            data={comentario.data}>
+            {comentario.mensagem}
+          </Comentario>
+        ))}
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './Comentario.css';
 
 // JSX -> html dentro do JS
-const Comentario = props => (
-    <div className="comentario">
-        <h2>{props.nome}:</h2>
-        <p>{props.email}</p>
-        <p>{props.children}</p>
-        <p>{props.data.toString()}</p>
-    </div>
-);
+const Comentario = props => {
+    return ( 
+        <div className="comentario">
+            <h2>{props.nome}:</h2>
+            <p>{props.email}</p>
+            <p>{props.children}</p>
+            <p>{props.data.toString()}</p>
+        </div>
+    )
+};
 
 
 export default Comentario
